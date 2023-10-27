8e4e9afa7a65f7c9c6d612725850df6ede3128bc
Ivanna <44304361+ivannarodriguez@users.noreply.github.com>
2023-10-27 22:53:45 +0000

    finished 2

--- app.rb
+++ app.rb
@@ -8,10 +8,29 @@ get("/") do
   "
 end
 
+# ---- square with form
 get("/square/new") do
   erb(:new_square_calc)
 end
 
 get("/square/results") do
+  @user_num = params[:users_number]
+  @square_user_num = params[:users_number].to_f**2
   erb(:square_results)
 end
+
+# ---- square root with form
+get("/square_root/new") do
+  erb(:new_square_root_calc)
+end
+
+get("/square_root/results") do
+  @user_num = params[:users_number].to_f
+  @square_root_user_num = Math.sqrt(@user_num)
+  erb(:square_root_results)
+end
+
+# ---- payment with form
+
+
+# ---- random with form
