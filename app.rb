90c7ae4bb0e443a4e394f01a02f828552dbdbf51
Ivanna <44304361+ivannarodriguez@users.noreply.github.com>
2023-10-28 02:27:28 +0000

    almost there

--- app.rb
+++ app.rb
@@ -25,12 +25,37 @@ get("/square_root/new") do
 end
 
 get("/square_root/results") do
-  @user_num = params[:users_number].to_f
-  @square_root_user_num = Math.sqrt(@user_num)
+  @user_num = params[:users_number]
+  @square_root_user_num = Math.sqrt(@user_num.to_f)
   erb(:square_root_results)
 end
 
 # ---- payment with form
+get("/payment/new") do
+  erb(:new_payment)
+end
 
+get("/payment/results") do
+  @apr = params[:user_apr].to_f
+  @years = params[:user_years].to_f
+  @principal = params[:user_pv].to_f.round(2)
+
+  monthly_interest_rate = (@apr / 12) / 100
+  total_payments = @years * 12
+  @payment = (@principal * monthly_interest_rate * (1 + monthly_interest_rate)**total_payments) / ((1 + monthly_interest_rate)**total_payments - 1)
+  @payment = @payment.round(2)
+ 
+  erb(:payment_results)
+end
 
 # ---- random with form
+get("/random/new") do
+  erb(:new_random)
+end
+
+get("/random/results") do
+  @min = params[:user_min].to_f
+  @max = params[:user_max].to_f
+  @random_number= rand(@min..@max)
+  erb(:random_result)
+end
